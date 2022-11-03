438fc4b670ca31e2cbb5acfce21a8d091a725e3e
sudoCss <mogo.gogo170@gmail.com>
2022-11-03 17:20:59 +0300

    A better setup to work better with legacy opengl

--- src/cpp/App.cpp
+++ src/cpp/App.cpp
@@ -8,13 +8,33 @@
 const GLuint DEFAULT_WINDOW_WIDTH = 800, DEFAULT_WINDOW_HEIGHT = 800;
 const char *WINDOW_TITLE = "OpenGL Application";
 
-void framebuffer_size_callback(GLFWwindow *, int new_screen_width, int new_screen_height)
+// This function is a replacement of gluPerspective
+void perspectiveGL(GLdouble fovY, GLdouble aspect, GLdouble zNear, GLdouble zFar)
 {
-    glViewport(0, 0, new_screen_width, new_screen_height);
+    const GLdouble pi = 3.1415926535897932384626433832795;
+    GLdouble fW, fH;
+
+    fH = tan(fovY / 360 * pi) * zNear;
+    fW = fH * aspect;
+
+    glFrustum(-fW, fW, -fH, fH, zNear, zFar);
+}
+
+void frame_buffer_size_callback(GLFWwindow *, int new_screen_width, int new_screen_height)
+{
+    glViewport(0, 0, new_screen_width, new_screen_height); // Tell OpenGL how to convert from coordinates to pixel values
+    glMatrixMode(GL_PROJECTION);                           // Switch to setting the camera perspective
+    glLoadIdentity();                                      // Reset the camera
+
+    perspectiveGL(45.0,                                                 // The camera angle
+                  (double)new_screen_width / (double)new_screen_height, // The width-to-height ratio
+                  1.0,                                                  // The near z clipping coordinate
+                  200.0);                                               // The far z clipping coordinate
 }
 
 int main()
 {
+    // Setup the window
     GLFWwindow *window;
 
     if (!glfwInit())
@@ -28,29 +48,34 @@ int main()
         return 1;
     }
 
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    glfwSetFramebufferSizeCallback(window, frame_buffer_size_callback);
     glfwMakeContextCurrent(window);
 
     glewExperimental = GL_TRUE;
 
     if (glewInit() != GLEW_OK)
         return 1;
+    // Done setting up the window
 
-    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+    // OpenGL hints
+    glEnable(GL_DEPTH_TEST);
 
     std::cout << "Loop Started" << std::endl;
     while (!glfwWindowShouldClose(window))
     {
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);               // Background color
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // Clear information from last draw
+        glMatrixMode(GL_MODELVIEW);                         // Switch to the drawing perspective
+        glLoadIdentity();                                   // Reset the drawing perspective
 
-        // Test (legacy OpenGL functions)
+        glTranslated(0, 0, -2); // Take the camera back two steps
+
         glBegin(GL_TRIANGLES);
         glColor3f(0.1, 0.2, 0.3);
-        glVertex2f(-0.5f, -0.5f);
-        glVertex2f(0.0f, 0.5f);
-        glVertex2f(0.5f, -0.5f);
+        glVertex3f(-0.5f, -0.5f, 0.0f);
+        glVertex3f(0.0f, 0.5f, 0.0f);
+        glVertex3f(0.5f, -0.5f, 0.0f);
         glEnd();
-        glFlush();
 
         glfwSwapBuffers(window);
         glfwPollEvents();
